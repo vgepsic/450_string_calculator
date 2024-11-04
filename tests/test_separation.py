6b203dea2b7f055defdb4d5cbb3b6430d3a0304c
Quentin Berthet <quentin@berthet.ch>
2024-11-04 09:40:15 +0100

    test_separation.py 201

---
+++ tests/test_separation.py
@@ -0,0 +1,15 @@
+import pytest
+from calculate.operators import Operators
+
+
+def test_separator():
+        # Arrange
+        input_numbers = "4+5"
+        expected_result = 9
+
+        # Act
+        result = Operators.Add(input_numbers)
+
+        # Assert
+        assert(result, expected_result)
+
