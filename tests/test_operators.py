385e63df0d9824e53441036caaf26adb6f20db85
DydyCraft-21 <114241438+DydyCraft-21@users.noreply.github.com>
2024-11-04 09:32:07 +0100

    Created the test_addition_with_zero unit test

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -12,4 +12,13 @@ def test_addition_should_make_simple_addition():
     # assert
     assert returned_value == expected_value
 
+def test_addition_with_zero():
+    # Arrange
+    operation = "7+0+8"
+    expected_value = 15
 
+    # Act
+    returned_value = Operators.Add(operation)
+
+    # Assert
+    assert returned_value == expected_value
