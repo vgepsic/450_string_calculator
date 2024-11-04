f94031eff689100c6cc6565743645cc7433f2977
CyrilPerrinjaquet <cyrilp.perso@gmail.com>
2024-11-04 09:32:25 +0100

    :white_check_mark: Added test for 302

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -12,4 +12,11 @@ def test_addition_should_make_simple_addition():
     # assert
     assert returned_value == expected_value
 
-
+def test_should_ignore_number_greater_than_1000_even_if_not_first_number():
+    # arrange
+    operation = "12+1002"
+    expected_value = 12
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
