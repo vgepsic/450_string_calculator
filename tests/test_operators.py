b8bc8a00cd13ae30fdd9beef71bb0ceebb451e47
Raph351 <marlo351.delapraz@gmail.com>
2024-11-04 14:20:36 +0100

    Test case 3

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -22,14 +22,3 @@ def test_imited_value_should_ignore_value_greater_than_1000():
     
     # assert
     assert returned_value == expected_value
-
-def test_imited_value_should_ignore_value_greater_than_1000():
-    # arrange
-    operation = "1001+10"
-    expected_value = 10
-
-    # act
-    returned_value = Operators.Add(operation)
-
-    # assert
-    assert returned_value == expected_value
