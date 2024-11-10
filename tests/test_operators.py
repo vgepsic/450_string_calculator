7b9512216de2f46b93ca072bd6eeafdf94e81b84
GreatBam <jonathan.gabioud@hotmail.com>
2024-11-10 11:12:27 +0100

    Ajout des test pourles critères d'acceptation phase 1

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -2,7 +2,8 @@ from calculate.operators import Operators
 
 # Addition Tests
 
-
+################# Critère d'acceptation phase 1 : somme correcte #################
+## Scénario 1 : somme de deux nombres
 def test_addition_should_make_simple_addition():
     # arrange
     operation = "7+8"
@@ -12,6 +13,26 @@ def test_addition_should_make_simple_addition():
     # assert
     assert returned_value == expected_value
 
+## Scénario 2 : somme de plus de deux nombres
+def test_addition_should_make_simple_addition_with_more_than_two_numbers():
+    # arrange
+    operation = "9+5+3+46+97+5"
+    expected_value = 165
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
+
+## Scénario 3 : somme du plus grand nombre admissible avec un autre chiffre valide
+def test_addition_should_make_simple_addition_with_the_highest_possible_number():
+    # arrange
+    operation = "999+1"
+    expected_value = 1000
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
+############################### Fin de la phase 1 ################################
 
 def test_addition_with_non_numerical_values():
     # arrange
