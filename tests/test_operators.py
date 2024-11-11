a83cf8a75848edbff8cb63e042039b1af73d4f82
Dylseat <dyl0301@protonmail.com>
2024-11-11 06:56:14 +0100

    add unit test for addition

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -12,4 +12,33 @@ def test_addition_should_make_simple_addition():
     # assert
     assert returned_value == expected_value
 
+def test_addition_with_other_separator_than_plus():
+    # arrange
+    operation = "5*5"
+    expected_value = 0
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
+
+def test_separator_plus_is_good():
+    # arrange
+    operation = "5+5"
+    expected_value = 10
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
+
+# Separator Tests
+
+def test_separator_when_too_much_separator_should_ignore_second_separator():
+    # arrange
+    operation = "27++38"
+    expected_value = 65
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
+
 
