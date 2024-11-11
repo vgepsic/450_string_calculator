218c019c3ff39c703336480968e58fc25e3ed27a
Dylseat <dyl0301@protonmail.com>
2024-11-11 07:03:06 +0100

    Code review

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -18,6 +18,31 @@ def test_addition_should_make_simple_addition_with_more_than_two_numbers():
     # arrange
     operation = "9+5+3+46+97+5"
     expected_value = 165
+    
+## Scénario 3 : somme du plus grand nombre admissible avec un autre chiffre valide
+def test_addition_should_make_simple_addition_with_the_highest_possible_number():
+    # arrange
+    operation = "999+1"
+    expected_value = 1000
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
+############################### Fin de la phase 1 ################################
+
+def test_addition_with_non_numerical_values():
+    # arrange
+    operation = "a+b"
+    expected_value = 0
+
+    # act
+    returned_value = Operators.Add(operation)
+
+    # assert
+    assert returned_value == expected_value
+
+    # groupe 2
+
 def test_addition_with_other_separator_than_plus():
     # arrange
     operation = "5*5"
@@ -44,28 +69,5 @@ def test_separator_when_too_much_separator_should_ignore_second_separator():
     expected_value = 65
     # act
     returned_value = Operators.Add(operation)
-    # assert
-    assert returned_value == expected_value
-
-## Scénario 3 : somme du plus grand nombre admissible avec un autre chiffre valide
-def test_addition_should_make_simple_addition_with_the_highest_possible_number():
-    # arrange
-    operation = "999+1"
-    expected_value = 1000
-    # act
-    returned_value = Operators.Add(operation)
-    # assert
-    assert returned_value == expected_value
-############################### Fin de la phase 1 ################################
-
-
-def test_addition_with_non_numerical_values():
-    # arrange
-    operation = "a+b"
-    expected_value = 0
-
-    # act
-    returned_value = Operators.Add(operation)
-
     # assert
     assert returned_value == expected_value
