0ba9731e8b56f2f4d844e0ec3dcaf255e04b35bc
vgepsic <113019726+vgepsic@users.noreply.github.com>
2024-12-08 11:55:23 +0100

    TDD iteration 1 : B (refactorized : SRP + denomination)

--- calculate/operators.py
+++ calculate/operators.py
@@ -1,8 +1,18 @@
 class Operators:
-    def Add(numbers):
-        parts = numbers.split('+')
-        sum = 0
-        for part in parts :
-            number = int(part)
-            sum += number
+    @staticmethod
+    def get_each_number(numbers: str):
+        return numbers.split('+')
+    
+    @staticmethod
+    def get_sum(numbers: list):
+        result = 0
+        for number in numbers:
+            x = int(number)
+            result += x
+        return result
+
+    @staticmethod
+    def Add(input_str):
+        numbers = Operators.get_each_number(input_str)
+        sum = Operators.get_sum(numbers)
         return sum
