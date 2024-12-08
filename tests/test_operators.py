3a9fb8e140aa4834ff5711384c958fcec1d44b51
vgepsic <113019726+vgepsic@users.noreply.github.com>
2024-12-08 11:51:29 +0100

    TDD iteration 1 : R (acceptance criteria 1&2)

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -3,5 +3,18 @@ import pytest
 
 # Addition Tests
 
-
+# TDD: iteration 1
+# arrange
+@pytest.mark.parametrize("operation, expected_value", [
+    ("7+8", 15),
+    ("1+2+3+4", 10),
+    ("1+2+0", 3),
+    ("0", 0),
+    ("1+2+3+4+5+6+7+8+9+10+11+12+300", 378),
+])
+def test_should_make_addition(operation, expected_value):
+    # act
+    returned_value = Operators.Add(operation)
+    # assert
+    assert returned_value == expected_value
 
